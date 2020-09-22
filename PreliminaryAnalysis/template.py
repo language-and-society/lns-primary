754001b83ba53e21914912628c11f9fcf94244e8
smartypants042 <tanishqchaudhary511@gmail.com>
2020-09-22 22:08:03 +0530

    Adding Initial Cleaning and Extraction Files for Dangal

---
+++ PreliminaryAnalysis/template.py
@@ -0,0 +1,18 @@
+# character_data = {
+#     'character_name': {
+#         'gender': 'F'
+#         'utterances': [
+#             "some stuff that is said", 
+#             "some other stuff that is said",
+#             "buh blah bhuh whatever",
+#         ],
+#     },
+# }
+
+# location_data = {
+#     'location_name': [
+#         "some stuff that is said", 
+#         "some other stuff that is said",
+#         "buh blah bhuh whatever",
+#     ],
+# }
