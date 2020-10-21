c228abe337d2d13f1049c5d68f187115bfa48450
prajneya <prajneya@gmail.com>
2020-10-21 09:02:52 +0530

    Add Wordclouds for all characters

---
+++ PreliminaryAnalysis/queen/queen_wordcloud.py
@@ -0,0 +1,34 @@
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+
+with open('queen_character_data.json') as f:
+  data = json.load(f)
+
+comment_words = ''
+stopwords = set(STOPWORDS)
+
+for key in data.keys():
+    for val in data[key]['utterances']:
+
+        val = str(val)
+        tokens = val.split()
+        for i in range(len(tokens)):
+            tokens[i] = tokens[i].lower()
+
+        comment_words += " ".join(tokens)+" "
+
+    wordcloud = WordCloud(width = 800, height = 800,
+                    background_color ='white',
+                    stopwords = stopwords,
+                    min_font_size = 10).generate(comment_words)
+
+    filename = "wordclouds/" + key + "_wordcloud.png"
+
+    plt.figure(figsize = (8, 8), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+
+    plt.savefig(filename)
